f64a32f8fd73b8a433e49a9463215662b52f1e96
Beta-Computer <67995456+Beta-Computer@users.noreply.github.com>
2022-01-12 09:29:16 +1300

    Update metroinfo.py

--- config/metroinfo.py
+++ config/metroinfo.py
@@ -1,15 +1,12 @@
 import requests
 import datetime
 from dateutil.parser import parse
+import Setup
 currenttime = datetime.datetime.now()
 baseurl = 'https://apis.metroinfo.co.nz/rti/siri/v1/sm?stopcode='
-# Change this \/ to a different code
-stopcode = '53088'
-# Change this \/ to your api token
-apitoken = '1279u3392r9dj9328sfj983289hmafjkaj821ujd291'
-# Change this to the bus number you would like the sensor to filter
-# Set filterbuscode = 0 if you would like to get the next bus regardless of bus code
-filterbuscode = '29'
+stopcode = Setup.stopcode
+apitoken = Setup.apitoken
+filterbuscode = Setup.filterbuscode
 nextbusnumber = 0
 headers = {'Ocp-Apim-Subscription-Key': apitoken}
 fullurl = baseurl + stopcode
