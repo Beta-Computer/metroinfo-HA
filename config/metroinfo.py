c5791d3adf79fdb0d9c57516ac348c81b8cf971b
Beta-Computer <67995456+Beta-Computer@users.noreply.github.com>
2022-01-19 17:19:58 +1300

    Update metroinfo.py

--- config/metroinfo.py
+++ config/metroinfo.py
@@ -7,18 +7,33 @@ baseurl = 'https://apis.metroinfo.co.nz/rti/siri/v1/sm?stopcode='
 stopcode = Setup.stopcode
 apitoken = Setup.apitoken
 filterbuscode = Setup.filterbuscode
-nextbusnumber = 0
+nextbus = 0
 headers = {'Ocp-Apim-Subscription-Key': apitoken}
 fullurl = baseurl + stopcode
 responsejson = (requests.get(fullurl, headers=headers)).json
-nextbuscode = responsejson()['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][nextbusnumber]['MonitoredVehicleJourney']['PublishedLineName']
-nextbustime = responsejson()['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][nextbusnumber]['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']
-if filterbuscode != 0:
-    while nextbuscode != filterbuscode:
-        nextbusnumber = int(nextbusnumber + 1)
-        nextbuscode = responsejson()['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][nextbusnumber]['MonitoredVehicleJourney']['PublishedLineName']
-        nextbustime = responsejson()['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][nextbusnumber]['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']
-    
+# Get data
+def getbustime(nextbusnumber):
+    nextbustime = responsejson()['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][int(nextbusnumber)]['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']
+    return nextbustime
+def getbuscode(nextbusnumber):
+    nextbuscode = responsejson()['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][int(nextbusnumber)]['MonitoredVehicleJourney']['PublishedLineName']
+    return nextbuscode
+# If get next bus is true it will get the next bus else it will look for the filtered bus
+if Setup.get_next_bus != "true":
+    nextbuscode = (getbuscode(nextbus))
+    if nextbuscode == Setup.filterbuscode:
+        nextbustime = (getbustime(nextbus))
+        nextbuscode = (getbuscode(nextbus))
+    else:
+        while nextbuscode != filterbuscode:
+            nextbus = nextbus + 1
+            nextbustime = (getbustime(nextbus))
+            nextbuscode = (getbuscode(nextbus))
+            print(nextbus)
+else:
+    nextbustime = (getbustime(nextbus))
+    nextbuscode = (getbuscode(nextbus))
+# Turn time of bus into time till bus
 Bustime = parse(nextbustime)
 Difftimemin = (Bustime.minute - currenttime.minute)
 Difftimehour = (Bustime.hour - currenttime.hour)
