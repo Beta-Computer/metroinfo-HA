bbc29ac18b156d0a0077871bfa8ab4cdce123960
Beta-Computer <67995456+Beta-Computer@users.noreply.github.com>
2022-02-05 21:42:29 +1300

    Fix error because no bus is coming.
    This should fix issue #3 it is not the best way we should probably look at the GTFS api by metroinfo to see when the next bus is scheduled and use that time like go.metroinfo.co https://go.metroinfo.co.nz

--- config/metroinfo.py
+++ config/metroinfo.py
@@ -10,33 +10,37 @@ headers = {'Ocp-Apim-Subscription-Key': Setup.apitoken}
 fullurl = baseurl + Setup.stopcode
 responsejson = (requests.get(fullurl, headers=headers)).json
 # Get data
+
 def getbustime(nextbusnumber):
     nextbustime = responsejson()['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][int(nextbusnumber)]['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']
     return nextbustime
+
 def getbuscode(nextbusnumber):
     nextbuscode = responsejson()['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][int(nextbusnumber)]['MonitoredVehicleJourney']['PublishedLineName']
     return nextbuscode
+
 # If get next bus is true it will get the next bus else it will look for the filtered bus
-if Setup.get_next_bus != "true":
-    nextbuscode = (getbuscode(nextbus))
-    if nextbuscode == Setup.filterbuscode:
-        nextbustime = (getbustime(nextbus))
+try:
+    if Setup.get_next_bus != "true":
         nextbuscode = (getbuscode(nextbus))
-    else:
-        while nextbuscode != filterbuscode:
-            nextbus = nextbus + 1
+        if nextbuscode == Setup.filterbuscode:
             nextbustime = (getbustime(nextbus))
             nextbuscode = (getbuscode(nextbus))
-            print(nextbus)
-else:
-    nextbustime = (getbustime(nextbus))
-    nextbuscode = (getbuscode(nextbus))
-# Turn time of bus into time till bus
-Bustime = parse(nextbustime)
-Difftimemin = (Bustime.minute - currenttime.minute)
-Difftimehour = (Bustime.hour - currenttime.hour)
-Difftimemin = Difftimemin + (Difftimehour * 60)
-print(Difftimemin)
-# Debug
-# print("time till next bus is:", Difftimemin, "minutes")
-# print("next bus code: " + nextbuscode)
+        else:
+            while nextbuscode != filterbuscode:
+                nextbus = nextbus + 1
+                nextbustime = (getbustime(nextbus))
+                nextbuscode = (getbuscode(nextbus))
+                print(nextbus)
+    else:
+        nextbustime = (getbustime(nextbus))
+        nextbuscode = (getbuscode(nextbus))
+    # Turn time of bus into time till bus
+    Bustime = parse(nextbustime)
+    Difftimemin = (Bustime.minute - currenttime.minute)
+    Difftimehour = (Bustime.hour - currenttime.hour)
+    Difftimemin = Difftimemin + (Difftimehour * 60)
+    print(Difftimemin)
+except KeyError:
+    # bus no bus detected at bus stop
+    print(0)
